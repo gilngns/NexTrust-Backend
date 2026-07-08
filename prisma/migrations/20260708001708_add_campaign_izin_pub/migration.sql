e0d68d68748b105eb3309d22fc28fe04984c8044
gilngns <nsgilang62@gmail.com>
2026-07-08 07:17:30 +0700

    add Campaign.izinPub column

---
+++ prisma/migrations/20260708001708_add_campaign_izin_pub/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - The values [PEMDA] on the enum `UserRole` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+ALTER TYPE "CampaignStatus" ADD VALUE 'EVALUATING';
+
+-- AlterEnum
+BEGIN;
+CREATE TYPE "UserRole_new" AS ENUM ('ADMIN', 'FOUNDATION', 'DINSOS', 'DONOR');
+ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "User" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
+ALTER TYPE "UserRole" RENAME TO "UserRole_old";
+ALTER TYPE "UserRole_new" RENAME TO "UserRole";
+DROP TYPE "UserRole_old";
+ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'FOUNDATION';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "Campaign" ADD COLUMN     "izinPub" TEXT;
